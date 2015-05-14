9e1b84f131f0460c681cbd77a000c8f856db33df
CodeFlowed <syncstep@gmail.com>
2015-05-14 22:35:30 +0000

    Add profile pic to About page, add form to Contribute link, cleanup quotes_controller.rb and delete new.html.erb because they're no longer needed

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,11 +1,7 @@
 class QuotesController < ApplicationController
 	def index
 		@quote = Quote.order("RANDOM()").first
-	end
-
-	def new
-		@quote = Quote.new
-	end
+	end	
 
 	def create
 		@quote = Quote.create(quote_params)
@@ -13,6 +9,9 @@ class QuotesController < ApplicationController
 			flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
 		end
 		redirect_to root_path
+	end
+
+	def about
 	end
 
 	private
